0c511b4bf515b0ebde7cd94438cad31430b6a317
bazhenova <anyusha2595@gmail.com>
2015-11-10 01:51:42 +0500

    Решение задачи

--- moment.js
+++ moment.js
@@ -3,18 +3,138 @@
 module.exports = function () {
     return {
         // Здесь как-то хранится дата ;)
-        date: null,
+        theDate: {},
+
+        get date() {
+            return getTimeInMinutes(this.theDate);
+        },
+
+        set date(newDate) {
+            this.theDate = parseDate(newDate);
+        },
 
         // А здесь часовой пояс
         timezone: null,
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            var hours = this.theDate.hours + this.timezone;
+            var day = this.theDate.day;
+            if (hours < 0) {
+                hours += 24;
+                day -= 1;
+            }
+            if (hours >= 24) {
+                hours -= 24;
+                day += 1;
+            }
+            var newDate = {
+                day: day,
+                hours: hours,
+                minutes: this.theDate.minutes
+            };
+            var strDate = getStrDate(newDate);
+            pattern = pattern.replace(/%DD/g, strDate.day);
+            pattern = pattern.replace(/%HH/g, strDate.hours);
+            pattern = pattern.replace(/%MM/g, strDate.minutes);
+            return pattern;
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+            var currentTime = this.date;
+            var momentTime = moment.date;
+            var diff = currentTime - momentTime;
+            if (diff > 0) {
+                var time = takeTime(diff);
+                return createStrTime(time);
+            } else {
+                return 'Ограбление уже идет!';
+            }
         }
     };
 };
+
+function parseDate(date) {
+    var daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+    var dayName = date.slice(0, 2);
+    var day = -1;
+    for (var i = 0; i < daysOfWeek.length; i++) {
+        if (dayName === daysOfWeek[i]) {
+            day = i;
+            break;
+        }
+    }
+    var zone = parseInt(date.slice(8));
+    var hours = parseInt(date.slice(3, 5)) - zone; // Перевели в +0
+    if (hours < 0) {
+        hours += 24;
+        day -= 1;
+    }
+    if (hours >= 24) {
+        hours -= 24;
+        day += 1;
+    }
+    day = (day < 0) ? 6 : day;
+    var minutes = parseInt(date.slice(6, 8));
+    return {
+        day: day,
+        hours: hours,
+        minutes: minutes
+    };
+}
+
+function getStrDate(date) {
+    var hours = (date.hours < 10) ? '0' + date.hours : date.hours;
+    var minutes = (date.minutes < 10) ? '0' + date.minutes : date.minutes;
+    var daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+    var day = daysOfWeek[date.day];
+    return {
+        day: day,
+        hours: hours,
+        minutes: minutes
+    };
+}
+
+function getTimeInMinutes(date) {
+    return (date.day * 24 + date.hours) * 60 + date.minutes;
+}
+
+function takeTime(minutes) {
+    var days = Math.floor(minutes / (60 * 24));
+    minutes -= 60 * 24 * days;
+    var hours = Math.floor(minutes / 60);
+    minutes -= 60 * hours;
+    return {
+        days: days,
+        hours: hours,
+        minutes: minutes
+    };
+}
+
+function createStrTime(time) {
+    var str = 'До ограбления ';
+    var rest = ['остался', 'осталось', 'осталось'];
+    var minutes = ['минута', 'минут', 'минуты'];
+    var hours = ['час', 'часов', 'часа'];
+    var days = ['день', 'дней', 'дня'];
+    var index = getCorrectIndex(time.days);
+    str += rest[index] + ' ';
+    str += time.days + ' ' + days[index] + ' ';
+    index = getCorrectIndex(time.hours);
+    str += time.hours + ' ' + hours[index] + ' ';
+    index = getCorrectIndex(time.minutes);
+    str += time.minutes + ' ' + minutes[index];
+    return str;
+}
+
+function getCorrectIndex(time) {
+    if (time !== 11 && time % 10 === 1) {
+        return 0;
+    }
+    if ((time < 12 || time > 14) && time % 10 >= 2 && time % 10 <= 4) {
+        return 2;
+    }
+    return 1;
+}
--- robbery.js
+++ robbery.js
@@ -10,6 +10,26 @@ module.exports.getAppropriateMoment = function (json, minDuration, workingHours)
     // 2. Находим подходящий ближайший момент начала ограбления
     // 3. И записываем в appropriateMoment
 
+    var data = JSON.parse(json);
+    var peopleTime = getPeopleTime(data);
+    var bankTimes = getBankTimes(workingHours);
+    var intersections = getIntersections(peopleTime, bankTimes);
+    if (intersections.length === 0) {
+        console.log('Нет подходящего времени.');
+        return appropriateMoment;
+    }
+    var neededTime = {};
+    for (var i = 0; i < intersections.length; i++) {
+        var intersection = intersections[i];
+        if (intersection.duration >= minDuration) {
+            neededTime = intersection;
+            break;
+        }
+    }
+    var date = getStrTime(takeTime(neededTime.from));
+    appropriateMoment.date = date;
+    var bankTimezone = parseInt(workingHours.from.slice(-2));
+    appropriateMoment.timezone = bankTimezone;
     return appropriateMoment;
 };
 
@@ -22,3 +42,116 @@ module.exports.getStatus = function (moment, robberyMoment) {
 
     return 'Ограбление уже идёт!';
 };
+
+function parseDate(date) {
+    var daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+    var day = daysOfWeek.indexOf(date.slice(0, 2));
+    var zone = parseInt(date.slice(8));
+    var hours = parseInt(date.slice(3, 5)) - zone; // Перевели в +0
+    if (hours < 0) {
+        hours += 24;
+        day -= 1;
+    }
+    if (hours >= 24) {
+        hours -= 24;
+        day += 1;
+    }
+    day = (day < 0) ? 6 : day;
+    var minutes = parseInt(date.slice(6, 8));
+    return {
+        day: day,
+        hours: hours,
+        minutes: minutes
+    };
+}
+
+function getTimeInMinutes(date) {
+    return (date.day * 24 + date.hours) * 60 + date.minutes; // считаем от начала недели
+}
+
+function getPeopleTime(data) {
+    var peopleTime = [];
+    var people = Object.keys(data);
+    for (var i = 0; i < people.length; i++) {
+        for (var j = 0; j < data[people[i]].length; j++) {
+            peopleTime.push({
+                from: getTimeInMinutes(parseDate(data[people[i]][j].from)),
+                to: getTimeInMinutes(parseDate(data[people[i]][j].to))
+            });
+        }
+    }
+    return peopleTime;
+}
+
+function getIntersections(peopleTime, bankTimes) {
+    var intersections = [];
+    for (var k = 0; k < bankTimes.length; k++) {
+        var duration = -1; // продолжительность в минутах
+        var bankTime = bankTimes[k];
+        var from = bankTime.from - 1;
+        var to = bankTime.from - 1;
+        var suitable = true;
+        for (var i = bankTime.from; i <= bankTime.to; i++) {
+            for (var index = 0; index < peopleTime.length; index++) {
+                var currentInterval = peopleTime[index];
+                if (currentInterval.from <= i && currentInterval.to >= i) {
+                    suitable = false;
+                    break;
+                }
+            }
+            if (suitable) {
+                duration += 1;
+                to += 1;
+            } else {
+                if (from !== to) {
+                    intersections.push({
+                        duration: duration,
+                        from: from,
+                        to: to
+                    });
+                }
+                from = i;
+                to = i;
+                duration = -1;
+                suitable = true;
+            }
+        }
+    }
+    return intersections;
+}
+
+function takeTime(minutes) {
+    var day = Math.floor(minutes / (60 * 24));
+    minutes -= 60 * 24 * day;
+    var hours = Math.floor(minutes / 60);
+    minutes -= 60 * hours;
+    return {
+        day: day,
+        hours: hours,
+        minutes: minutes
+    };
+}
+
+function getStrTime(date) {
+    var hours = (date.hours < 10) ? '0' + date.hours : date.hours;
+    var minutes = (date.minutes < 10) ? '0' + date.minutes : date.minutes;
+    var daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+    var day = daysOfWeek[date.day];
+    return day + ' ' + hours + ':' + minutes + '+0';
+}
+
+function getBankTimes(workingHours) {
+    var times = [];
+    var daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+    for (var i = 0; i < daysOfWeek.length; i++) {
+        var from = daysOfWeek[i] + ' ' + workingHours.from;
+        from = getTimeInMinutes(parseDate(from));
+        var to = daysOfWeek[i] + ' ' + workingHours.to;
+        to = getTimeInMinutes(parseDate(to));
+        times.push({
+            from: from,
+            to: to
+        });
+    }
+    return times;
+}
